da32cf60276d02e6f6b96c2c180e7f465d7ad9d9
nguyenthdat <nguyenthdat@outlook.com>
2022-01-07 10:56:54 +0000

    added code

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	cards := deck{newcard(), newcard()}
 	cards = append(cards, "HAHAHAHAHA")
 	cards.print()
+	fmt.Println([]byte("Hi There"))
+
+	greeding := "Hi Jason"
+	fmt.Println([]byte(greeding))
+
 }
 
 func newcard() string {
