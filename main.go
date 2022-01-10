19726c3c41d874a4f2574ade21f7e5324dfc7236
nguyenthdat <nguyenthdat@outlook.com>
2022-01-10 05:09:18 +0000

    updated

--- main.go
+++ main.go
@@ -1,15 +1,1 @@
 package main
-
-func main() {
-	//cards := newDeck()
-	//hand, remaincards := deal(cards, 5)
-	//hand.print()
-	//remaincards.print()
-	// fmt.Println(cards.toString())
-	//cards.savetofile("My_cards")
-	//cards := newdeckfromfile("My_cards")
-	//cards.print()
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
-}
