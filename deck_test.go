fd44acb286fa502a47340b85cb9eeefd2e8099c3
nguyenthdat <nguyenthdat@outlook.com>
2022-01-10 04:41:48 +0000

    updated

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,10 @@
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 16 {
+		t.Errorf("Expecting deck has 16 cards, but got %v cards", len(d))
+	}
+}
