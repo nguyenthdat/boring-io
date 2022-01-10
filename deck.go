d8c0627d72c4226dc93e43f0cfb11ad8c5705270
nguyenthdat <nguyenthdat@outlook.com>
2022-01-10 03:47:17 +0000

    add newDeck function

--- deck.go
+++ deck.go
@@ -4,6 +4,18 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Rings"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, suite, " of ", value)
+		}
+	}
+	return cards
+}
+
 // reciever function
 func (d deck) print() {
 	for i, card := range d {
