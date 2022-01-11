78793e065f013b20d4d35a1515bd8bd1005470bc
nguyenthdat <nguyenthdat@outlook.com>
2022-01-11 04:52:48 +0000

    updated

--- main.go
+++ main.go
@@ -5,15 +5,22 @@ import (
 )
 
 func main() {
-	colors := make(map[int]string)
+	//colors := make(map[int]string)
 	// var colors map[string]string
-	/*
-		colors := map[string]string{
-			"red":   "#ff000",
-			"green": "#4bf745",
-		}
-	*/
-	colors[10] = "#fffff"
-	delete(colors, 10)
+
+	colors := map[string]string{
+		"red":   "#ff000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+	//colors[10] = "#fffff"
+	//delete(colors, 10)
 	fmt.Println(colors)
+	printmap(colors)
+}
+
+func printmap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code of", color, "is", hex)
+	}
 }
