5da9b6a8ebc60e260f5d47b1b290b6be4248ebba
nguyenthdat <nguyenthdat@outlook.com>
2022-01-13 11:23:19 +0000

    updated

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+	for _, l := range links {
+		checklink(l)
+	}
+}
+
+func checklink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "errorrr!")
+		return
+	}
+	fmt.Println(link, "OK")
 }
