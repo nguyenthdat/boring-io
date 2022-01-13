e2e2fc9497ce1efc52ef37183bdb8e1376ad3330
nguyenthdat <nguyenthdat@outlook.com>
2022-01-13 09:58:37 +0000

    updated

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
+}
 
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (eb englishBot) getGreeting() string {
