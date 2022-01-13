2cc29df39833655415041320db2ff5a5ef781803
nguyenthdat <nguyenthdat@outlook.com>
2022-01-13 11:17:04 +0000

    updated

--- main.go
+++ main.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
-
-type logWriter struct{}
-
 func main() {
-	resp, err := http.Get("https://google.com/")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	/*
-		bs := make([]byte, 99999)
-		resp.Body.Read(bs)
-		fmt.Println(string(bs))
-	*/
-	lw := logWriter{}
-	io.Copy(lw, resp.Body)
-}
-func (logWriter) Write(bs []byte) (int, error) {
-	fmt.Println(string(bs))
-	fmt.Println("Number of write bytes", len(bs))
-	return len(bs), nil
+
 }
