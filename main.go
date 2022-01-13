6dd41c21bfa4b3df1090d08f33ac23e33eded8d7
nguyenthdat <nguyenthdat@outlook.com>
2022-01-13 11:51:43 +0000

    updated

--- main.go
+++ main.go
@@ -15,16 +15,20 @@ func main() {
 		"http://www.udemy.com",
 		"http://youtube.com",
 	}
+	c := make(chan string)
 	for _, l := range links {
-		go checklink(l)
+		go checklink(l, c)
 	}
+	fmt.Println(<-c)
 }
 
-func checklink(link string) {
+func checklink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "errorrr!")
+		c <- "errorrrrrrrr!"
 		return
 	}
 	fmt.Println(link, "OK")
+	c <- "OK"
 }
